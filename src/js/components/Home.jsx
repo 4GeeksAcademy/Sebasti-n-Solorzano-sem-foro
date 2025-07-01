a545813ba541f12ffc8568a455be67530d908c1a
sebastian-solorzano95 <sebassolorzano95@gmail.com>
2025-07-01 16:38:56 +0000

    proyecto-semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import  { useState } from "react";
 import '../../styles/index.css'
 //include images into your bundle
 
 
 //create your first component
 const Home = () => {
+	const [ luzColor, setLuzColor] = useState("rojo")
 	return (
 		<div className="contenedor">
 
 			<div className="cable"></div>
 			
 			<div className="caja-principal">
-				<div className="color"></div>
-				<div className="color"></div>
-				<div className="color"></div>
+				<div className={"color rojo " + (luzColor=="rojo" ? "on" : "")} onClick={() => setLuzColor("rojo")}></div>
+				<div className={"color amarillo " + (luzColor=="amarillo" ? "on" : "")} onClick={() => setLuzColor("amarillo")}></div>
+				<div className={"color verde " + (luzColor=="verde" ? "on" : "")} onClick={() => setLuzColor("verde")}></div>
 			</div>
             
 
